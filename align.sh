2a22ae7df08f933cf195ef47ec85c09eaac11667
Joshua Tauberer <jt@occams.info>
2015-06-10 08:13:01 -0400

    let align.sh be invoked from any directory, it will figure out where it is

--- align.sh
+++ align.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 # Usage: ./align.sh audio.wav transcript.txt
 
-SPHINX=sphinx4
+HERE=$(dirname $0)
+SPHINX=$HERE/sphinx4
 JAR=$SPHINX/sphinx4-core/target/sphinx4-core-1.0-SNAPSHOT.jar
-MODEL=cmusphinx-en-us-5.2
 DICTIONARY=$SPHINX/sphinx4-data/src/main/resources/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict
+MODEL=$HERE/cmusphinx-en-us-5.2
 
-java -cp .:$JAR:opencsv-3.3.jar \
+if [ -z "$1" ]; then
+	echo "Usage: ./align.sh sample.wav sample.txt"
+	exit 1
+fi
+
+# Go.
+java -cp $HERE:$JAR:$HERE/opencsv-3.3.jar \
 	Aligner \
 	$MODEL \
 	$DICTIONARY \
